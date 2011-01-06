65c9e7c46f3b85537a30a9e692f2de1ee8d833df
Lim Chee Kin <limcheekin@vobject.com>
2011-01-06 06:53:02 +0000

    Format the code of jquery.ui.widget.template.js properly.

--- src/js/templates/jquery.ui.widget.template.js
+++ src/js/templates/jquery.ui.widget.template.js
@@ -19,60 +19,60 @@
  */
 
 var NameSpace = {
-	ClassName: {
-		options: { // default options. values are stored in widget's prototype
+  ClassName: {
+    options: { // default options. values are stored in widget's prototype
       option1: "optionValue"		
-        },
+    },
     _create: function() {
     	// called on construction
     	this.log('NameSpace.ClassName._create called. this.options.option1 = ' + this.options.option1);
-        },
-		_init: function() {
+    },
+	_init: function() {
 			// called on construction and re-initialization
-			this.log('NameSpace.ClassName._init called.');
-			this.method1('calling from NameSpace.ClassName._init');
-		},        
-		destroy: function() {
-    	// called on removal
-			this.log('NameSpace.ClassName.destroy called.');
-        },
+		this.log('NameSpace.ClassName._init called.');
+		this.method1('calling from NameSpace.ClassName._init');
+	},        
+	destroy: function() {
+        // called on removal
+		this.log('NameSpace.ClassName.destroy called.');
+    },
     // logging to the firebug's console, put in 1 line so it can be removed easily for production
     log: function($message) { if (window.console && window.console.log) window.console.log($message); },
-		method1: function(params) {
+	method1: function(params) {
     	// plugin specific method
-			this.log('NameSpace.ClassName.method1 called. params = ' + params);
-        }
-	}
+		this.log('NameSpace.ClassName.method1 called. params = ' + params);
+    }
+  }
 };
 
 $.widget('ui.className', NameSpace.ClassName);
 
 // extends/inherits from superclass: ClassName
 var NameSpace = {
-		ExtendedClassName: $.extend({}, $.ui.className.prototype, {
-			options: { // default options. values are stored in widget's prototype
-	      option1: "ExtendedClassName.optionValue"		
-	        },
-	    _create: function() {
-	      $.ui.className.prototype._create.call(this); // call the superclass's _create function
-	      // ExtendedClassName's construction code here
-	      this.log('NameSpace.ExtendedClassName._create called. this.options.option1 = ' + this.options.option1);
-	        },
-			_init: function() {
-			  $.ui.className.prototype._init.call(this); // call the superclass's _init function
-			  // ExtendedClassName's construction and re-initialization code here	
-			  this.log('NameSpace.ExtendedClassName._init called.');
-			},        
-			destroy: function() {
-	    	// ExtendedClassName's destroy code here
-				this.log('NameSpace.ExtendedClassName.destroy called.');
-				$.ui.className.prototype.destroy.call(this); // call the superclass's destroy function
-	        },
-			method1: function(params) {
-	    	// plugin specific method
-				this.log('NameSpace.ExtendedClassName.method1 called. params = ' + params);
-	        }
-		})
+  ExtendedClassName: $.extend({}, $.ui.className.prototype, {
+    options: { // default options. values are stored in widget's prototype
+	  option1: "ExtendedClassName.optionValue"		
+    },
+	_create: function() {
+	  $.ui.className.prototype._create.call(this); // call the superclass's _create function
+	  // ExtendedClassName's construction code here
+	  this.log('NameSpace.ExtendedClassName._create called. this.options.option1 = ' + this.options.option1);
+    },
+    _init: function() {
+	  $.ui.className.prototype._init.call(this); // call the superclass's _init function
+	  // ExtendedClassName's construction and re-initialization code here	
+      this.log('NameSpace.ExtendedClassName._init called.');
+    },        
+	destroy: function() {
+	  // ExtendedClassName's destroy code here
+	  this.log('NameSpace.ExtendedClassName.destroy called.');
+	  $.ui.className.prototype.destroy.call(this); // call the superclass's destroy function
+    },
+	method1: function(params) {
+      // plugin specific method
+	  this.log('NameSpace.ExtendedClassName.method1 called. params = ' + params);
+    }
+  })
 };
 
 $.widget('ui.extendedClassName', NameSpace.ExtendedClassName);
