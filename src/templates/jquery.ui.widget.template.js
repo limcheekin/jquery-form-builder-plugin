53e6fbd0e89d81fa68afca2711b410f2b39b0538
Lim Chee Kin <limcheekin@vobject.com>
2011-01-10 04:08:50 +0000

    Revised JQuery UI Widget Template.

--- src/templates/jquery.ui.widget.template.js
+++ src/templates/jquery.ui.widget.template.js
@@ -21,62 +21,64 @@
 var NameSpace = {
   ClassName: {
     options: { // default options. values are stored in widget's prototype
-      option1: "optionValue"		
+      publicOption: "public option value",
+      _privateOption: "private option value"		
     },
   _create: function() {
     	// called on construction
-    	this.log('NameSpace.ClassName._create called. this.options.option1 = ' + this.options.option1);
+    	this._log('NameSpace.ClassName._create called. this.options.publicOption = ' + this.options.publicOption);
     },
 	_init: function() {
 			// called on construction and re-initialization
-		this.log('NameSpace.ClassName._init called.');
+		this._log('NameSpace.ClassName._init called.');
 		this.method1('calling from NameSpace.ClassName._init');
 	},        
 	destroy: function() {
         // called on removal
-		this.log('NameSpace.ClassName.destroy called.');
+		this._log('NameSpace.ClassName.destroy called.');
 		
 		// From: http://blog.nemikor.com/2010/05/15/building-stateful-jquery-plugins/
 		// call the base destroy function.
 		$.Widget.prototype.destroy.call(this);		
     },
     // logging to the firebug's console, put in 1 line so it can be removed easily for production
-    log: function($message) { if (window.console && window.console.log) window.console.log($message); },
+  _log: function($message) { if (window.console && window.console.log) window.console.log($message); },
 	method1: function(params) {
     	// plugin specific method
-		this.log('NameSpace.ClassName.method1 called. params = ' + params);
+		this._log('NameSpace.ClassName.method1 called. params = ' + params);
     }
   }
 };
 
-$.widget('ui.className', NameSpace.ClassName);
+$.widget('ns.className', NameSpace.ClassName); // ns: namespace, don't use "ui" as it was reserved by JQuery UI
 
 // extends/inherits from superclass: ClassName
 var NameSpace = {
-  ExtendedClassName: $.extend({}, $.ui.className.prototype, {
+  ExtendedClassName: $.extend({}, $.ns.className.prototype, {
     options: { // default options. values are stored in widget's prototype
-	  option1: "ExtendedClassName.optionValue"		
+      publicOption: "public option value",
+      _privateOption: "private option value"		
     },
 	_create: function() {
-	  $.ui.className.prototype._create.call(this); // call the superclass's _create function
+	  $.ns.className.prototype._create.call(this); // call the superclass's _create function
 	  // ExtendedClassName's construction code here
-	  this.log('NameSpace.ExtendedClassName._create called. this.options.option1 = ' + this.options.option1);
+	  this._log('NameSpace.ExtendedClassName._create called. this.options.publicOption = ' + this.options.publicOption);
     },
-    _init: function() {
-	  $.ui.className.prototype._init.call(this); // call the superclass's _init function
+  _init: function() {
+	  $.ns.className.prototype._init.call(this); // call the superclass's _init function
 	  // ExtendedClassName's construction and re-initialization code here	
-      this.log('NameSpace.ExtendedClassName._init called.');
+      this._log('NameSpace.ExtendedClassName._init called.');
     },        
 	destroy: function() {
 	  // ExtendedClassName's destroy code here
-	  this.log('NameSpace.ExtendedClassName.destroy called.');
-	  $.ui.className.prototype.destroy.call(this); // call the superclass's destroy function
+	  this._log('NameSpace.ExtendedClassName.destroy called.');
+	  $.ns.className.prototype.destroy.call(this); // call the superclass's destroy function
     },
 	method1: function(params) {
       // plugin specific method
-	  this.log('NameSpace.ExtendedClassName.method1 called. params = ' + params);
+	  this._log('NameSpace.ExtendedClassName.method1 called. params = ' + params);
     }
   })
 };
 
-$.widget('ui.extendedClassName', NameSpace.ExtendedClassName);
+$.widget('ns.extendedClassName', NameSpace.ExtendedClassName); // ns: namespace, don't use "ui" as it was reserved by JQuery UI
